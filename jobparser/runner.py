7b4de27f1a27337f2c99e7edcbb6650b522a6f48
AntonP <poslavskyanton87@gmail.com>
2022-03-25 19:22:31 +0700

    Done using CrawlerRunner

--- jobparser/runner.py
+++ jobparser/runner.py
@@ -1,8 +1,28 @@
-from scrapy.crawler import CrawlerProcess
+import scrapy
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.supjobru import SupjobruSpider
+
+
+
+configure_logging()
+settings = get_project_settings()
+runner = CrawlerRunner(settings)
+runner.crawl(HhruSpider)
+runner.crawl(SupjobruSpider)
+d = runner.join()
+d.addBoth(lambda _: reactor.stop())
+
+reactor.run()
+"""from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from jobparser import settings
 from jobparser.spiders.hhru import HhruSpider
 from jobparser.spiders.supjobru import SupjobruSpider
+
 
 
 if __name__ == '__main__':
@@ -12,3 +32,4 @@ if __name__ == '__main__':
     crawler_process.crawl(HhruSpider)
     crawler_process.crawl(SupjobruSpider)
     crawler_process.start()
+"""
